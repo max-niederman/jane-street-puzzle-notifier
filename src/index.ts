3cc4745a40a19246ad9e095687b27979c03f411d
Max Niederman <max@maxniederman.com>
2024-12-11 02:37:47 -0800

    implement discord notifications

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
+import { Client, Events, GatewayIntentBits, TextChannel } from "discord.js";
 import { Persist, type SubscriptionType } from "./persist";
+import { diffResults, scrape } from "./scrape";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const persist = new Persist();
@@ -58,6 +59,83 @@ client.on(Events.InteractionCreate, async (interaction) => {
   }
 });
 
-await persist.load();
+console.log("Performing initial scrape...");
+let results = { name: "", leaderboard: [] };
+console.log("Initial scrape completed with results: ", results);
 
+async function scrapeAndNotify() {
+  console.log("Scraping...");
+  const newResults = await scrape();
+  console.log("Scrape completed with results: ", newResults);
+
+  const diff = diffResults(results, newResults);
+  if (diff.nameChanged) {
+    console.log("Puzzle name changed!");
+    for (const [channelID, subscriptions] of Object.entries(
+      persist.subscriptions
+    )) {
+      const channel: TextChannel = await client.channels.fetch(channelID);
+
+      if (subscriptions.includes("puzzle")) {
+        await channel.send(
+          `🧩 [The puzzle](<https://www.janestreet.com/puzzles/current-puzzle/>) name has changed! It's now: ${newResults.name}`
+        );
+      }
+    }
+  }
+
+  if (diff.leaderboard.added.length || diff.leaderboard.removed.length) {
+    console.log("Leaderboard changed!");
+    for (const [channelID, subscriptions] of Object.entries(
+      persist.subscriptions
+    )) {
+      const channel: TextChannel = await client.channels.fetch(channelID);
+
+      if (subscriptions.includes("leaderboard")) {
+        const files = [
+          {
+            name: "leaderboard.txt",
+            description: "New leaderboard",
+            attachment: Buffer.from(newResults.leaderboard.join("\n")),
+          },
+        ];
+
+        const added = diff.leaderboard.added
+          .map((name) => `+ ${name}`)
+          .join("\n");
+        const removed = diff.leaderboard.removed
+          .map((name) => `\\- ${name}`)
+          .join("\n");
+
+        if (added.length + removed.length > 1000) {
+          files.push({
+            name: "diff.txt",
+            description: "Difference between the old and new leaderboards",
+            attachment: Buffer.from(`${added}\n${removed}`),
+          });
+
+          await channel.send({
+            content:
+              "📈 The leaderboard has changed! There are too many changes to summarize here, but a diff is attached.",
+            files,
+          });
+          continue;
+        }
+
+        await channel.send({
+          content: `📈 Leaderboard changes:\n${added}\n${removed}`,
+          files,
+        });
+      }
+    }
+  }
+
+  results = newResults;
+}
+
+await persist.load();
 client.login(process.env.DISCORD_TOKEN);
+setInterval(
+  scrapeAndNotify,
+  Number(process.env.SCRAPE_INTERVAL ?? "60") * 1000
+);
